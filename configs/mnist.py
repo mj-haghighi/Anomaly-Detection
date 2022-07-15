d6192ce64a542cfd608242571a4f7e327cf1bb7b
mj-haghighi <mohammadjavad.developer@gmail.com>
2022-07-15 11:12:57 +0430

    Add 'mean' and 'std' of 'mnist' dataset in configs

--- configs/mnist.py
+++ configs/mnist.py
@@ -6,6 +6,8 @@ class Config(IConfig):
     filetype = EXT.zip
     datatype = EXT.png
     classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    mean = (0.1307,)
+    std = (0.3081,)
     trainset='training/'
     validationset='testing/'
     testset='testing/'
