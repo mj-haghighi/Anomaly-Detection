d8bf47aca6b1adbfdcdefc9a80f1fb9b81ff6128
mj-haghighi <mohammadjavad.developer@gmail.com>
2022-07-15 19:14:02 +0430

    Implement calidation procedure using 'v_loader' as validation dataloader

--- train/trainer.py
+++ train/trainer.py
@@ -14,17 +14,20 @@ class Trainer:
         v_loader,
         optimizer,
         num_epochs,
-        metrics: List[IMetric],
+        t_metrics: List[IMetric],
+        v_metrics: List[IMetric],
         loggers: List[ILogger]
         ) -> None:
 
         self.model = model
         self.error = error
         self.device = device
-        self.metrics = metrics
+        self.t_metrics = t_metrics
+        self.v_metrics = v_metrics
         self.t_loader = t_loader
         self.v_loader = v_loader
-        self.dynamics = Dynamics()
+        self.t_dynamics = Dynamics()
+        self.v_dynamics = Dynamics()
         self.optimizer = optimizer
         self.num_epochs = num_epochs
         self.loggers = loggers
@@ -33,22 +36,41 @@ class Trainer:
         print('training start ...')
         self.model.to(self.device)
         for epoch in range(self.num_epochs):
-            self.dynamics.epoch = epoch
-            self.dynamics.iteration = -1
+            self.t_dynamics.epoch = epoch
+            self.t_dynamics.iteration = -1
+            self.v_dynamics.epoch = epoch
+            self.v_dynamics.iteration = -1
+
+            # train            
             self.model.train()
             for idx, data, labels in self.t_loader:
-                self.dynamics.iteration +=1
+                self.t_dynamics.iteration +=1
                 self.optimizer.zero_grad()
                 data = data.to(self.device)
                 prediction_values = self.model(data) # (B, C)
                 prediction_probs = softmax(prediction_values, dim=1) # (B, C)
                 loss = self.error(prediction_probs, labels)
-                self.dynamics.b_loss = loss.item()
+                self.t_dynamics.b_loss = loss.item()
                 loss.backward()
                 self.optimizer.step()
+            
+                train_result = (prediction_probs, labels, idx)
+                for metric in self.t_metrics:
+                    metric.calculate(self.t_dynamics, *train_result)
+                
+            # validation
+            self.model.eval()
+            for idx, data, labels in self.v_loader:
+                self.v_dynamics.iteration +=1
+                data = data.to(self.device)
+                prediction_values = self.model(data) # (B, C)
+                prediction_probs = softmax(prediction_values, dim=1) # (B, C)
+                loss = self.error(prediction_probs, labels)
+                self.v_dynamics.b_loss = loss.item()
 
-                for metric in self.metrics:
-                    metric.calculate(self.dynamics, prediction_probs, labels, idx)
+                validation_result = (prediction_probs, labels, idx)
+                for metric in self.v_metrics:
+                    metric.calculate(self.v_dynamics, *validation_result)
 
             for logger in self.loggers:
-                logger.log(self.dynamics, self.metrics)
+                logger.log(self.t_dynamics, self.v_dynamics, self.t_metrics, self.v_metrics)
