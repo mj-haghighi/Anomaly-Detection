f95f29c7e01c04d1cf117fd22cf3cbd408eecc7c
mj-haghighi <mohammadjavad.developer@gmail.com>
2022-07-16 11:22:42 +0430

    Implement 'MINMetricValueModelSaver' and 'MAXMetricValueModelSaver' in best value model saver

---
+++ saver/best_model.py
@@ -0,0 +1,29 @@
+import math
+import torch.nn as nn
+from metric.metric_interface import IMetric
+from .saver_interface import IModelSaver
+
+class MAXMetricValueModelSaver(IModelSaver):
+    def __init__(self, model: nn.Module, savedir:str, helper_in_compare = None, lock_on: IMetric = None) -> None:
+        super().__init__(model, savedir, helper_in_compare, lock_on)
+        self.best_value = -math.inf
+
+    def look_for_save(self, metric_value=None):
+        if self.locked_metric is not None:
+            metric_value = self.helper(self.locked_metric.value)
+        if metric_value > self.best_value:
+            self.best_value = metric_value
+            super().save_model()
+
+
+class MINMetricValueModelSaver(IModelSaver):
+    def __init__(self, model: nn.Module, savedir:str, helper_in_compare = None, lock_on: IMetric = None) -> None:
+        super().__init__(model, savedir, helper_in_compare, lock_on)
+        self.best_value = math.inf
+
+    def look_for_save(self, metric_value=None):
+        if self.locked_metric is not None:
+            metric_value = self.helper(self.locked_metric.value)
+        if metric_value < self.best_value:
+            self.best_value = metric_value
+            super().save_model()
