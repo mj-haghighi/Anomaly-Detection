746a3fe7af0e19ede5608eba9c078fdec673b4b8
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-11-04 16:54:25 +0330

    Bugfix: amdend datapath in cifar10 reformer

--- reform/cifar10.py
+++ reform/cifar10.py
@@ -31,7 +31,7 @@ def reform_datset(
         data_dir: str
 ):
 
-    data_batches_path = osp.join(data_dir, 'data_batch_.*')
+    data_batches_path = osp.join(data_dir, 'data_batch_*')
     test_batch_path = osp.join(data_dir, 'test_batch')
     meta_path = osp.join(data_dir, 'batches.meta')
 
@@ -52,7 +52,8 @@ def reform_datset(
         if not osp.isdir(osp.join(reform_dir, test, label)):
             os.makedirs(osp.join(reform_dir, test, label))
 
-    data_files = glob.glob(data_batches_path)
+    data_files = sorted(glob.glob(data_batches_path))
+
     for file_path in data_files:
         file = open(file_path, mode='rb')
         content = pickle.load(file, encoding='bytes')
@@ -62,8 +63,12 @@ def reform_datset(
         batch_data = content[b'data']
         file_names = content[b'filenames']
 
-        write_images(batch_labels, batch_data, file_names,
-                     count=num_cases_per_batch, base_dir=osp.join(reform_dir, train), label_names=label_names)
+        write_images(base_dir=osp.join(reform_dir, train),
+                     batch_labels=batch_labels,
+                     batch_data=batch_data,
+                     file_names=file_names,
+                     count=num_cases_per_batch,
+                     label_names=label_names)
 
     file = open(test_batch_path, mode='rb')
     content = pickle.load(file, encoding='bytes')
