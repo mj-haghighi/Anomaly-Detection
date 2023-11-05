688a96604c35793a3502958479e673e52848f291
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-11-05 19:17:47 +0330

    Add 'mean', 'std' and 'validatoin' to cifar10 config

--- configs/cifar10.py
+++ configs/cifar10.py
@@ -15,8 +15,9 @@ class Config(IConfig):
     raw_data_folder = 'cifar-10-batches-py'
     classes = ['airplane', 'automobile', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-    mean = 'todo'
-    std = 'todo'
+    mean = [0.49139968, 0.48215827, 0.44653124]
+    std = [0.24703233, 0.24348505, 0.26158768]
     trainset = 'train/'
     testset = 'test/'
+    validationset = 'validation/'
     reform = reform_cifar10
