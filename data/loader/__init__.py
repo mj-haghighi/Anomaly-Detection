f01ce971d514f4971ac4456c6f2274b52bc8427f
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-11-05 19:52:36 +0330

    Implement 'dataloader' for 'cifar10'

--- data/loader/__init__.py
+++ data/loader/__init__.py
@@ -1,7 +1,9 @@
 from typing import Dict
 from .mnist import collate_fn as mnist_collate_fn
+from .cifar10 import collate_fn as cifar10_collate_fn
 from enums import DATASETS
 
 collate_fns = {
-    DATASETS.mnist: mnist_collate_fn
+    DATASETS.mnist: mnist_collate_fn,
+    DATASETS.cifar10: cifar10_collate_fn
 }
---
+++ data/loader/cifar10.py
@@ -0,0 +1,16 @@
+from typing import List
+import torch
+import numpy as np
+
+
+def collate_fn(batch):
+    img_names: List[str] = []
+    imgs: List[torch.Tensor] = []
+    clabels: List[torch.Tensor] = []
+    for sample in batch:
+        img_name, img, clabel = sample
+        img_names.append(img_name)
+        imgs.append(img)
+        clabels.append(clabel)
+
+    return img_names, torch.stack(imgs, 0), torch.stack(clabels, 0)
