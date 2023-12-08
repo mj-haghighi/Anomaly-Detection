c10b2be4f8267554fc17502fe321c33264e65269
mj-haghighi <mohammadjavad.developer@gmail.com>
2023-12-08 14:51:58 +0330

    Implenment 'run' in 'ILogger' to use logger in multithread behavior

--- logger/logger_interface.py
+++ logger/logger_interface.py
@@ -1,8 +1,22 @@
+import time
+from queue import Queue
 from typing import List
 from train.dynamics import Dynamics
 from metric.metric_interface import IMetric
 
 
 class ILogger:
-    def log(self,fold: int, epoch: int, samples: List[str], phase: str, labels: List[str], true_labels: List[str], metrics: List[IMetric]):
+    def __init__(self, logQ: Queue):
+        self.logQ = logQ
+
+    def log(self, fold: int, epoch: int, samples: List[str], phase: str, labels: List[str], true_labels: List[str], metrics: List[IMetric]):
         raise Exception("This is not implemented")
+
+    def start(self):
+        while True:
+            time.sleep(1)
+            if not self.logQ.empty():
+                log_item = self.logQ.get()
+            if log_item == "EOF":
+                break
+                self.log(**log_item)
