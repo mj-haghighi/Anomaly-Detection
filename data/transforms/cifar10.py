c31dc9c61402b39d25c2b6788e597baaabc00966
mj-haghihgi <mohammadjavad.developer@gmail.com>
2023-12-15 15:43:17 +0330

    Change order of transforms

--- data/transforms/cifar10.py
+++ data/transforms/cifar10.py
@@ -4,9 +4,9 @@ from configs.cifar10 import Config
 
 t_transforms = tv_transforms.Compose([
     tv_transforms.ToTensor(),
+    transforms.RandomHorizontalFlip(),
+    tv_transforms.RandomAffine(translate=(10, 10), scale=(0.85, 1.15)),
     tv_transforms.Normalize(mean=Config.mean, std=Config.std),
-    tv_transforms.RandomRotation(degrees=15),
-    tv_transforms.RandomAffine(translate=(10, 10), scale=(0.85, 1.15))
 ])
 
 v_transforms = tv_transforms.Compose([
