9cb7ab10e69f9a432f53047c8e7d1418bdf3b3a3
mj-haghihgi <mohammadjavad.developer@gmail.com>
2023-12-15 15:46:57 +0330

    Add 'weight_decay=5e-4' to sgd optimizer

--- optimizer/__init__.py
+++ optimizer/__init__.py
@@ -5,16 +5,14 @@ from enums import OPTIMIZER
 
 
 def load(name: str = "adam", model: Module = None, learning_rate: float = 0.0001):
-    print('name: ', name)
     if OPTIMIZER.adam == name:
         return optim.Adam(model.parameters(), lr=learning_rate)
     
     if OPTIMIZER.rmsprobe == name:
         return optim.RMSprop(model.parameters(), lr=learning_rate)
     
-    print('OPTIMIZER.sgd: ', OPTIMIZER.sgd)
     if OPTIMIZER.sgd == name:
-        return optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+        return optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
     
     if OPTIMIZER.sparseadam == name:
         return optim.SparseAdam(model.parameters(), lr=learning_rate)
