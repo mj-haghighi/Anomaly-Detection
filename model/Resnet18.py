5b99d3e17fd1f93bbaf78a9468e841e43ddb78ff
mj-haghihgi <mohammadjavad.developer@gmail.com>
2023-12-15 15:46:08 +0330

    Abort freezing backbone in models

--- model/Resnet18.py
+++ model/Resnet18.py
@@ -8,11 +8,6 @@ class Resnet18(nn.Module):
         super(Resnet18, self).__init__()
         self.resnet18 = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 
-        # Freeze all layers except the final classification layer
-        for param in self.resnet18.parameters():
-            param.requires_grad = False
-        self.resnet18.fc.requires_grad = True
-
         # Modify the final classification layer for the number of classes in CIFAR-10
         in_features = self.resnet18.fc.in_features
         self.resnet18.fc = nn.Linear(in_features, num_classes)
--- model/Resnet34.py
+++ model/Resnet34.py
@@ -7,11 +7,6 @@ class Resnet34(nn.Module):
         super(Resnet34, self).__init__()
         self.resnet = models.resnet34(pretrained=True)
 
-        # Freeze all layers except the final classification layer
-        for param in self.resnet.parameters():
-            param.requires_grad = False
-        self.resnet.fc.requires_grad = True
-
         # Modify the final classification layer for the number of classes in CIFAR-10
         in_features = self.resnet.fc.in_features
         self.resnet.fc = nn.Linear(in_features, num_classes)
