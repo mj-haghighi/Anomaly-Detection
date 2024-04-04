c356dfdd4bf31b61ebe59148041a581003c88ed1
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-04 17:35:18 +0330

    Amend 'collate_fn' in dataloaders to care about sample index in dataset instead of sample name

--- data/loader/mnist.py
+++ data/loader/mnist.py
@@ -3,13 +3,13 @@ import torch
 import numpy as np
 
 def collate_fn(batch):
-    img_names: List[str] = []
+    indices: List[int] = []
     imgs: List[torch.Tensor] = []
     clabels: List[torch.Tensor] = []
     for sample in batch:
-        img_name, img, clabel = sample
-        img_names.append(img_name)
+        index, img, clabel = sample
+        indices.append(index)
         imgs.append(img)
         clabels.append(clabel)
 
-    return img_names, torch.stack(imgs, 0), torch.stack(clabels, 0)
+    return indices, torch.stack(imgs, 0), torch.stack(clabels, 0)
