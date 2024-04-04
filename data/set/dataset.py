b8b3b73d7784403c04e5a5fcfad4a16c3c3ae4cc
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-04 17:41:27 +0330

    Implement 'GeneralDataset'

---
+++ data/set/dataset.py
@@ -0,0 +1,46 @@
+import glob
+import torch
+import pandas as pd
+import os.path as osp
+from PIL import Image
+from enums import PHASE
+from enums import DATASETS
+from utils import to_categorical
+from configs import configs
+from data.set.dataset_interface import IDataset
+from torch.utils.data import Dataset as TorchDataset
+
+
+class GeneralDataset(TorchDataset, IDataset):
+    def __init__(self, dataset_name: str, label_column, phase, transform=None):
+        self.dataset_name = dataset_name
+        self.label_column = label_column
+        self.samples = self.__collect_samples()
+        self.transform = transform
+        self.phase = phase
+        self.dataset_config = configs[dataset_name]
+        print('{} sample available in {} set'.format(self.dataset_length, phase))
+
+    def __len__(self):
+        return self.dataset_length
+
+    def __getitem__(self, idx):
+        index, img_path, label = self.samples[idx]
+        clabel = to_categorical.sample(label, self.dataset_config.labels)
+        clabel = torch.Tensor(clabel)
+        img = Image.open(img_path)
+
+        if self.transform:
+            img = self.transform(img)
+        return index, img, clabel
+
+    def __collect_samples(self):
+        dataset_info_path = osp.join(self.datset_config.outdir, self.datset_name, 'info.csv')
+        df = pd.read_csv(dataset_info_path)
+        df = df[df['phase'] == self.phase]
+        if self.label_column in df.columns:
+            indices, paths, labels = df.index.values, df['path'], df[self.label_column]
+            self.dataset_length = len(df)
+            return zip(indices, paths, labels)
+        else:
+            raise(f"label_column: {self.label_column} is not valid")
