42b99ba98ac212392a3ba1d0b9a977f60a68346f
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-12 14:11:17 +0330

    Bugfix: train once model on data with np=0.0

--- train_model_with_all_options_on_noisy_data.py
+++ train_model_with_all_options_on_noisy_data.py
@@ -1,13 +1,14 @@
 import subprocess
 
-MODELS = ["resnet18", "resnet34", "xception"]
+# MODELS = ["resnet18", "resnet34", "xception"]
+MODELS = ["xception"]
 DATASETS = ["cifar10"]#,"mnist", , "cifar100"]
 LR_SCHEDULER = ['reduceLR', 'none', 'cosine_annealingLR']
 PARAMS = ['pretrain', 'kaiming_normal']
-OPTIMIZER_LR = [('adam', "0.0001"), ('sgd', "0.001"), ('rmsprobe', "0.0001"), ('sparseadam', "0.0001")]
+OPTIMIZER_LR = [('adam', "0.001"), ('sgd', "0.1")]#, ('rmsprobe', "0.001"), ('sparseadam', "0.001")]
 NOISE_PERSENTAGE_OPTIONS = ["0.0", "0.03", "0.07", "0.13"]
-NOISE_SPARSITY_OPTIONS = ["0.0", "0.2", "0.4", "0.6"]
-
+NOISE_SPARSITY_OPTIONS = ["0.0", "0.2", "0.6"]
+EPOCH = "5"
 # Path to your Python script
 SCRIPT_PATH = "train.py"
 
@@ -17,17 +18,15 @@ for model in MODELS:
             for params in PARAMS:
                 for opt, lr in OPTIMIZER_LR:
                     for np in NOISE_PERSENTAGE_OPTIONS:
-                        for ns in NOISE_SPARSITY_OPTIONS:
-                            print(f"Running script for  model: {model}, dataset: {dataset}, lr_scheduler: {lr_scheduler}, params: {params}, opt: {opt}, ns:{ns}, np:{np}")
-                            
-                            # Build command to run the script
+                        if np == "0.0":
+                            ns = "0.0"
                             command = [
                                 "python", SCRIPT_PATH,
                                 "--model", model,
                                 "--dataset", dataset,
                                 "--lr_scheduler", lr_scheduler,
                                 "--params", params,
-                                "--epochs", "100",
+                                "--epochs", EPOCH,
                                 "--batch_size", "64",
                                 "--folds", "3",
                                 "--lr", lr,
@@ -37,8 +36,30 @@ for model in MODELS:
                                 "--noise_percentage", np,
                                 "--noise_sparsity", ns
                             ]
-                            
+                            print(command)
                             # Execute the command and wait for it to finish
                             subprocess.run(command, check=True)
 
-                            print(f"Completed running script for model: {model}, dataset: {dataset}, lr_scheduler: {lr_scheduler}, params: {params}, opt: {opt}, ns:{ns}, np:{np}")
+                        else:
+                            for ns in NOISE_SPARSITY_OPTIONS:                                
+                                # Build command to run the script
+                                command = [
+                                    "python", SCRIPT_PATH,
+                                    "--model", model,
+                                    "--dataset", dataset,
+                                    "--lr_scheduler", lr_scheduler,
+                                    "--params", params,
+                                    "--epochs", EPOCH,
+                                    "--batch_size", "64",
+                                    "--folds", "3",
+                                    "--lr", lr,
+                                    "--logdir", "logs/",
+                                    "--device", "cuda:0",
+                                    "--optimizer", opt,
+                                    "--noise_percentage", np,
+                                    "--noise_sparsity", ns
+                                ]
+                                print(command)
+                                # Execute the command and wait for it to finish
+                                subprocess.run(command, check=True)
+
