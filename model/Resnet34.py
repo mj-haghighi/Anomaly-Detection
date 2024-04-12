e8d178eafb167b3eadf28445b92e5b7385c20b1c
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-12 09:12:11 +0330

    Bugfix: replace 'resnet' and 'resnet18' in 'resnet34' model

--- model/Resnet34.py
+++ model/Resnet34.py
@@ -9,16 +9,16 @@ class Resnet34(nn.Module):
     def __init__(self, num_classes, pretrain=True):
         super(Resnet34, self).__init__()
         if pretrain:
-            self.resnet = models.resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
+            self.resnet34 = models.resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
         else:
-            self.resnet = models.resnet34()
+            self.resnet34 = models.resnet34()
 
         # Modify the final classification layer for the number of classes in CIFAR-10
-        in_features = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(in_features, num_classes)
+        in_features = self.resnet34.fc.in_features
+        self.resnet34.fc = nn.Linear(in_features, num_classes)
 
     def load_state_dict(self, state_dict):
-        self.resnet18.load_state_dict(state_dict)
+        self.resnet34.load_state_dict(state_dict)
 
     def forward(self, x):
-        return self.resnet(x)
+        return self.resnet34(x)
