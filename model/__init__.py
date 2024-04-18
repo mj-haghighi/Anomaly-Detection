8eb0006259dc378cb75a936451577b9695dd4397
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-18 18:13:00 +0330

    Implement 'get' function in 'model' to get model by name and set parameters

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,6 @@
 from typing import Dict
+from utils.params import init_kaiming_normal
+
 import torch.nn as nn
 from enums import MODELS
 from .Resnet18 import Resnet18
@@ -10,3 +12,12 @@ models: Dict[str, nn.Module] = {
     MODELS.resnet34: Resnet34,
     MODELS.xception: Xception
 }
+
+
+def get(name: str, num_classes: int, pretrain: bool = False):
+    if pretrain:
+        return models[name](num_classes=num_classes, pretrain=True)
+    else:
+        model = models[name](num_classes=num_classes, pretrain=False)
+        init_kaiming_normal(model)
+        return model
