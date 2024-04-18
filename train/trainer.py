166a9ef9d5f5595aeab5523fd7520d223d45f49c
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-18 21:00:24 +0330

    Use 'verbose' to controle verbose level in training and validating process

--- train/trainer.py
+++ train/trainer.py
@@ -53,39 +53,43 @@ def train_one_epoch(fold, epoch, model, dataloader, optimizer, queue):
                 "labels": np.argmax(labels.cpu().detach().numpy(), axis=1),
                 "metrics": metrics_result
                 })
-        print('TRAIN Fold %d Epoch: %d Iter: %d' % (fold, epoch, iteration))
+        verbose('TRAIN Fold {} Epoch: {} Iter: {} Loss: {:2f}'.format(fold, epoch, iteration, loss_all), VERBOSE.LEVEL_3)
         iteration += 1
+    return np.mean(epoch_loss)
 
-def validate_one_epoch(fold, epoch, model, dataloader, optimizer, queue):
+def validate_one_epoch(fold, epoch, model, dataloader, queue):
     model.eval()
     epoch_loss = []
-    iteration = 0 
-    for idx, data, labels in dataloader:
-        data, labels = data.to(device), labels.to(device)
-        prediction_values = model(data)  # (B, C)
-        prediction_probs = softmax(prediction_values, dim=1)  # (B, C)
+    iteration = 0
+    with torch.no_grad():
+        for idx, data, labels in dataloader:
+            time.sleep(0.015)
+            data, labels = data.to(device), labels.to(device)
+            prediction_values = model(data)  # (B, C)
+            prediction_probs = softmax(prediction_values, dim=1)  # (B, C)
 
-        loss_each = error(prediction_values, labels)
-        loss_all = torch.mean(loss_each)
-        epoch_loss.append(loss_all.item())
-        loss_all.backward()
-        optimizer.step()
+            loss_each = error(prediction_values, labels)
+            loss_all = torch.mean(loss_each)
+            epoch_loss.append(loss_all.item())
 
-        metrics_result = calculate_metrics(prediction_probs, labels, loss_each)
-
-        queue.put({
-            "fold": fold, "epoch": epoch, "iteration": iteration,
-            "samples": copy.deepcopy(idx), "phase": PHASE.VALIDATION,
-            "labels": np.argmax(labels.cpu().detach().numpy(), axis=1),
-            "metrics": metrics_result
-        })
-        print('VALIDATION Fold %d Epoch: %d Iter: %d' % (fold, epoch, iteration))
-        iteration += 1
+            metrics_result = calculate_metrics(prediction_probs, labels, loss_each)
+            if queue.qsize() > 30:
+                while queue.qsize() > 1:
+                    time.sleep(0.01)
+            queue.put({
+                "fold": fold, "epoch": epoch, "iteration": iteration,
+                "samples": copy.deepcopy(idx), "phase": PHASE.VALIDATION,
+                "labels": np.argmax(labels.cpu().detach().numpy(), axis=1),
+                "metrics": metrics_result
+            })
+            verbose('VALIDATION Fold {} Epoch: {} Iter: {} Loss: {:.2f}'.format(fold, epoch, iteration, loss_all), VERBOSE.LEVEL_3)
+            iteration += 1
     return np.mean(epoch_loss)
 
 
 
 def train_fold(num_folds, num_epochs, fold, model_name, dataset_name, optimizer_name, learning_rate, num_classes, pretrain, label_column, logdir, lr_scheduler_name, queue):
+    fold_start_time = time.time()
     model = predefined_models.get(name=model_name, num_classes=num_classes, pretrain=pretrain)
     model.to(device)
     optimizer = predifined_optimizers.get(name=optimizer_name, model=model, learning_rate=learning_rate)
@@ -106,17 +110,16 @@ def train_fold(num_folds, num_epochs, fold, model_name, dataset_name, optimizer_
     model_savers = [best_model.MINMetricValueModelSaver(savedir=logdir)]
 
     for epoch in range(num_epochs):
-        # print("Im here2")
-        train_one_epoch(fold, epoch, model, trainloader, optimizer, queue)
-        # print("Im here3")
-        loss = validate_one_epoch(fold, epoch, model, validationloader, optimizer, queue)
-        # print("Im here4") 
-        lr_scheduler.take_step(metrics=loss)
-        # print("Im here5")
+        epoch_start_time = time.time()
+        train_loss = train_one_epoch(fold, epoch, model, trainloader, optimizer, queue)
+        val_loss = validate_one_epoch(fold, epoch, model, validationloader, queue)
+        lr_scheduler.take_step(metrics=val_loss)
         for saver in model_savers:
-        #     print("Im here6")
-            saver.look_for_save(metric_value=loss, epoch=epoch, fold=fold, model=model)
-    print(f"Fold {fold} Finish!")
+            saver.look_for_save(metric_value=val_loss, epoch=epoch, fold=fold, model=model)
+        
+        verbose('Fold {} Epoch: {}  TrainLoss: {:.2f} ValLoss: {:.2f} Time: {} s'.format(fold, epoch, train_loss, val_loss, time.time() - epoch_start_time), VERBOSE.LEVEL_2)
+
+    verbose('Model {} Fold {} Time: {} s'.format(model_name, fold, time.time() - fold_start_time), VERBOSE.LEVEL_1)
     queue.put(None)
 
 def logger(logdir, model_name, optimizer_name, queue):
