fbbe5a091c438b765ed291ba1aaaa003c9e9c261
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-23 19:41:06 +0330

    Amend 'train_model_with_all_options_on_noisy_data' to use 'examines' and run undone examines

--- train_model_with_all_options_on_noisy_data.py
+++ train_model_with_all_options_on_noisy_data.py
@@ -1,65 +1,33 @@
 import subprocess
+import pandas as pd
 
-# MODELS = ["resnet18", "resnet34", "xception"]
-MODELS = ["xception"]
-DATASETS = ["cifar10"]#,"mnist", , "cifar100"]
-LR_SCHEDULER = ['reduceLR', 'none', 'cosine_annealingLR']
-PARAMS = ['pretrain', 'kaiming_normal']
-OPTIMIZER_LR = [('adam', "0.001"), ('sgd', "0.1")]#, ('rmsprobe', "0.001"), ('sparseadam', "0.001")]
-NOISE_PERSENTAGE_OPTIONS = ["0.0", "0.03", "0.07", "0.13"]
-NOISE_SPARSITY_OPTIONS = ["0.0", "0.2", "0.6"]
-EPOCH = "5"
+EPOCH = "15"
+FOLD = "3"
+
 # Path to your Python script
 SCRIPT_PATH = "train.py"
+cols = [ "dataset", "model", "optim", "init", "lr_scheduler", "np", "ns", "lr" ]
 
-for model in MODELS:
-    for dataset in DATASETS:
-        for lr_scheduler in LR_SCHEDULER:
-            for params in PARAMS:
-                for opt, lr in OPTIMIZER_LR:
-                    for np in NOISE_PERSENTAGE_OPTIONS:
-                        if np == "0.0":
-                            ns = "0.0"
-                            command = [
-                                "python", SCRIPT_PATH,
-                                "--model", model,
-                                "--dataset", dataset,
-                                "--lr_scheduler", lr_scheduler,
-                                "--params", params,
-                                "--epochs", EPOCH,
-                                "--batch_size", "64",
-                                "--folds", "3",
-                                "--lr", lr,
-                                "--logdir", "logs/",
-                                "--device", "cuda:0",
-                                "--optimizer", opt,
-                                "--noise_percentage", np,
-                                "--noise_sparsity", ns
-                            ]
-                            print(command)
-                            # Execute the command and wait for it to finish
-                            subprocess.run(command, check=True)
+df = pd.read_csv("/home/vision/Repo/cleanset/logs/examins.csv", index_col='index')
+df = df[(df['done'] == False) & (df['model'] == 'xception')]
 
-                        else:
-                            for ns in NOISE_SPARSITY_OPTIONS:                                
-                                # Build command to run the script
-                                command = [
-                                    "python", SCRIPT_PATH,
-                                    "--model", model,
-                                    "--dataset", dataset,
-                                    "--lr_scheduler", lr_scheduler,
-                                    "--params", params,
-                                    "--epochs", EPOCH,
-                                    "--batch_size", "64",
-                                    "--folds", "3",
-                                    "--lr", lr,
-                                    "--logdir", "logs/",
-                                    "--device", "cuda:0",
-                                    "--optimizer", opt,
-                                    "--noise_percentage", np,
-                                    "--noise_sparsity", ns
-                                ]
-                                print(command)
-                                # Execute the command and wait for it to finish
-                                subprocess.run(command, check=True)
+for index, row in df.head(10).iterrows():
+    command = [
+        "python", SCRIPT_PATH,
+        "--model", row['model'],
+        "--dataset", row['dataset'],
+        "--lr_scheduler", row['lr_scheduler'],
+        "--params", row['init'],
+        "--epochs", EPOCH,
+        "--batch_size", "64",
+        "--folds", FOLD,
+        "--lr", row['lr'][3:],
+        "--logdir", "logs/",
+        "--device", "cuda:0",
+        "--optimizer", row['optim'],
+        "--noise_percentage", row['np'][3:],
+        "--noise_sparsity", row['ns'][3:]
+    ]
+    print('command: ', command)
+    subprocess.run(command, check=True)
 
