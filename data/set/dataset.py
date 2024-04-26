0b16677de8e865157a8d554788a54b97651bd350
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-26 21:07:28 +0330

    Amend 'GeneralDataset' to apply 'data_filtering_policy' on dataset

--- data/set/dataset.py
+++ data/set/dataset.py
@@ -3,18 +3,19 @@ import pandas as pd
 import os.path as osp
 from PIL import Image
 from enums import PHASE
+from utils import to_categorical
 from configs import configs
 from torch.utils.data import Dataset as TorchDataset
-from utils import to_categorical
 
 class GeneralDataset(TorchDataset):
-    def __init__(self, dataset_name: str, label_column: str, phase: str, transform=None):
-        self.phase          = phase
-        self.dataset_name   = dataset_name
-        self.label_column   = label_column
-        self.dataset_config = configs[dataset_name]
-        self.transform      = transform
-        self.samples        = self.__collect_samples()
+    def __init__(self, dataset_name: str, label_column: str, phase: str, transform=None, data_filtering_policy=None):
+        self.phase              = phase
+        self.dataset_name       = dataset_name
+        self.label_column       = label_column
+        self.dataset_config     = configs[dataset_name]
+        self.transform          = transform
+        self.filtering_policy   = data_filtering_policy
+        self.samples            = self.__collect_samples()
         print('{} sample available in {} set'.format(self.dataset_length, phase))
 
     def __len__(self):
@@ -34,9 +35,17 @@ class GeneralDataset(TorchDataset):
         dataset_info_path = osp.join(self.dataset_config.outdir, self.dataset_name, 'info.csv')
         df = pd.read_csv(dataset_info_path, index_col='index')
         df = df[df['phase'] == self.phase]
+        if self.filtering_policy is not None:
+            filter_out_samples = self.filtering_policy.filter_samples()
+            print(f"number of filtered samples: {len(filter_out_samples)}")
+            print(f"number of samples: {len(df)}")
+            df = df[~df.index.isin(filter_out_samples.index)]
+            print(f"number of samples after filtering: {len(df)}")
+
         if self.label_column in df.columns:
             indices, paths, labels = df.index.values, df['path'], df[self.label_column]
             self.dataset_length = len(df)
             return list(zip(indices, paths, labels))
         else:
-            raise(f"label_column: {self.label_column} is not valid")
+
+            raise Exception(f"label_column: {self.label_column} is not valid")
