dabe324e16be516586f1d8e7d189556cc3495656
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-04-26 21:02:24 +0330

    Amend train and trainer to work with experiment number and load experiment information from dataframes

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import time
 import torch
 import os.path as osp
+import pandas as pd
 import argparse
 import torch.nn as nn
 from torch import multiprocessing
@@ -8,25 +9,16 @@ from multiprocessing import Queue
 from metric.level1 import Loss, Proba
 from utils.inject_noise_to_dataset import NOISE_PERSENTAGE_OPTIONS, NOISE_SPARSITY_OPTIONS
 from train.trainer import train_fold, logger
+from configs.general import FILTERING_EXPERIMENT_BASE_DIR, EXPERIMENT_BASE_DIR, EXPERIMENT_COLS, FILTERING_EXPERIMENT_INFO_PATH, EXPERIMENT_INFO_PATH
 
 if __name__ == '__main__':
     multiprocessing.set_start_method('spawn', force=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='start training on dataet')
-    parser.add_argument('--dataset',            type=str,   default=None,               choices=['mnist', 'cifar10', 'cifar100'], help='choose dataset')
-    parser.add_argument('--model',              type=str,   default='resnet18',         choices=['resnet18', 'resnet34', 'xception'], help='choose model')
-    parser.add_argument('--lr_scheduler',       type=str,   default='none',             choices=['none', 'cosine_annealingLR', 'reduceLR'], help='choose learning rate scheduler')
-    parser.add_argument('--params',             type=str,   default='pretrain',    choices=['pretrain', 'kaiming_normal'], help='choose params initialization')
-    parser.add_argument('--epochs',             type=int,   default=100,        help='max number of epochs')
-    parser.add_argument('--batch_size',         type=int,   default=64,         help='batch size')
-    parser.add_argument('--folds',              type=int,   default=3,          help='number of folds in cross validation')
-    parser.add_argument('--lr',                 type=float, default=0.0001,     help='learning rate')
-    parser.add_argument('--logdir',             type=str,   default='logs/',    help='log directory')
-    parser.add_argument('--device',             type=str,   default='cpu',              choices=['cpu', 'cuda:0', 'cuda:1', 'cuda:2', 'cuda:3', 'cuda:4', 'cuda:5', 'cuda:6'], help='learning device')
-    parser.add_argument('--optimizer',          type=str,   default='sgd',              choices=['adam', 'sgd', 'rmsprobe', 'sparseadam'], help='choose model optimizer')
-    parser.add_argument('--noise_percentage',   type=float, default=0.0,                choices=NOISE_PERSENTAGE_OPTIONS, help='injected noise precentage of dataset')
-    parser.add_argument('--noise_sparsity',     type=float, default=0.0,                choices=NOISE_SPARSITY_OPTIONS, help='sparsity of injected noise to the dataset (fraction of off-diagonal zeros in noise matrix)')
+    
+    parser.add_argument('--experiment_number',           type=int, help='experiment index in experiments dataset')
+    parser.add_argument('--filtering_experiment_number', type=int, help='experiment index in filtering experiments dataset')
 
     args = parser.parse_args()
     return args
@@ -38,45 +30,45 @@ queue_s = [multiprocessing.Queue() for i in range(3)]
 # Use multiprocessing to train each fold in parallel
 if __name__ == '__main__':
     args = parse_args()
-    logdir = osp.join(
-        args.logdir,
-        args.dataset,
-        args.model,
-        args.optimizer,
-        args.params,
-        args.lr_scheduler,
-        f'np={args.noise_percentage}',
-        f'ns={args.noise_sparsity}',
-        f'lr={args.lr}')
+
+    filtering_policy = None
+    based_on = None
+    if args.filtering_experiment_number is not None:
+        filtering_experiments = pd.read_csv(FILTERING_EXPERIMENT_INFO_PATH, index_col='index')
+        target_filtering_experiment = filtering_experiments.iloc[args.filtering_experiment_number]
+        experiment_number = target_filtering_experiment['basic_experiment_index']
+        filtering_policy = target_filtering_experiment['data_filtering_policy']
+        retrieval_policy = target_filtering_experiment['data_retrieval_policy']
+        based_on = target_filtering_experiment['based_on']
+        experiment_dir = osp.join(FILTERING_EXPERIMENT_BASE_DIR, str(experiment_number), based_on, filtering_policy, retrieval_policy) 
+    else:
+        experiment_number = args.experiment_number
+        experiment_dir = osp.join(EXPERIMENT_BASE_DIR, *[str(target_experiment[col]) for col in EXPERIMENT_COLS])
+    
+    experiments = pd.read_csv(EXPERIMENT_INFO_PATH, index_col='index')
+    target_experiment = experiments.iloc[experiment_number]
+    folds = target_experiment['folds']
 
     processes = []
-    for fold in range(args.folds):
+    for fold in range(folds):
         process_args = {
-            'num_folds': args.folds,
-            'num_epochs': args.epochs,
             'fold': fold,
-            'model_name': args.model,
-            'dataset_name': args.dataset,
-            'optimizer_name': args.optimizer,
-            'learning_rate': args.lr,
-            'lr_scheduler_name': args.lr_scheduler,
-            'num_classes': 10,
-            'logdir': logdir,
-            'pretrain': args.params=='pretrain',
             'queue': queue_s[fold],
-            'label_column': f"noisy_label[np={args.noise_percentage},ns={args.noise_sparsity}]" if args.noise_percentage > 0.001 else 'true_label'
+            'experiment_number': experiment_number,
+            'filtering_policy': filtering_policy,
+            'based_on': based_on,
+            'experiment_dir': experiment_dir
         }
         p = multiprocessing.Process(target=train_fold, kwargs=process_args)
         processes.append(p)
         p.start()
-    
+
     log_processes = []
-    for fold in range(args.folds):
+    
+    for fold in range(folds):
         log_process_args = {
             'queue': queue_s[fold],
-            'logdir': logdir,
-            'model_name': args.model ,
-            'optimizer_name': args.optimizer
+            'logdir': experiment_dir
         }
         logp = multiprocessing.Process(target=logger, kwargs=log_process_args)
         log_processes.append(logp)
@@ -87,4 +79,3 @@ if __name__ == '__main__':
 
     for p in log_processes:
         p.join()
-    
--- train/trainer.py
+++ train/trainer.py
@@ -3,12 +3,15 @@ import time
 import torch
 import copy
 import numpy as np
+import pandas as pd
 import torch.nn as nn
 import optimizer as predifined_optimizers
 import model as predefined_models
 from utils.verbose import verbose
 from saver import best_model
 from enums import PHASE, VERBOSE
+from configs import configs
+from configs.general import EXPERIMENT_INFO_PATH, FILTERING_EXPERIMENT_INFO_PATH, FILTERING_EXPERIMENT_BASE_DIR, EXPERIMENT_BASE_DIR, EXPERIMENT_COLS
 from data.set import Subset
 from data.set import GeneralDataset
 from lrscheduler import get_lrscheduler
@@ -18,6 +21,7 @@ from logger.dataframe import DataframeLogger
 from torch.nn.functional import softmax
 from sklearn.model_selection import KFold
 from saver.last_model import LastEpochModelSaver
+from data.filtering_policy import get_data_filtering_policy 
 
 metrics = [Loss(), Proba()]
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -89,14 +93,42 @@ def validate_one_epoch(fold, epoch, model, dataloader, queue):
 
 
 
-def train_fold(num_folds, num_epochs, fold, model_name, dataset_name, optimizer_name, learning_rate, num_classes, pretrain, label_column, logdir, lr_scheduler_name, queue):
+def train_fold(fold, queue, experiment_number, filtering_policy=None, based_on=None, experiment_dir=None):
+    data_filtering_policy = None
+    base_dir = EXPERIMENT_BASE_DIR
+    if filtering_policy is not None:
+        filtering_experiments = pd.read_csv(FILTERING_EXPERIMENT_INFO_PATH, index_col='index')
+        data_filtering_policy = get_data_filtering_policy(
+            policy_name=filtering_policy,
+            metric_name=based_on,
+            experiment_base_dir=EXPERIMENT_BASE_DIR,
+            experiment_number=experiment_number, 
+            experiments_info_path=EXPERIMENT_INFO_PATH,
+            experiments_dataset_columns=EXPERIMENT_COLS)
+
+    experiments       = pd.read_csv(EXPERIMENT_INFO_PATH, index_col='index')
+    target_experiment = experiments.iloc[experiment_number]
+    num_folds         = target_experiment['folds']
+    num_epochs        = target_experiment['epochs']
+    model_name        = target_experiment['model']
+    dataset_name      = target_experiment['dataset']
+    optimizer_name    = target_experiment['optim']
+    learning_rate     = float(target_experiment['lr'][3:])
+    pretrain          = target_experiment['init'] == 'pretrain'
+    num_classes       = len(configs[dataset_name].classes)
+    label_column      = f"noisy_label[{target_experiment['np']},{target_experiment['ns']}]" if float(target_experiment['np'][3:]) > 0.001 else 'true_label'
+    lr_scheduler_name = target_experiment['lr_scheduler']
+
     fold_start_time = time.time()
     model = predefined_models.get(name=model_name, num_classes=num_classes, pretrain=pretrain)
     model.to(device)
     optimizer = predifined_optimizers.get(name=optimizer_name, model=model, learning_rate=learning_rate)
     lr_scheduler = get_lrscheduler(optimizer, lr_scheduler_name)
 
-    dataset = GeneralDataset(dataset_name=dataset_name, label_column=label_column, phase=PHASE.TRAIN, transform=None)
+    dataset = GeneralDataset(
+        dataset_name=dataset_name, label_column=label_column, transform=None,
+        phase=PHASE.TRAIN, data_filtering_policy=data_filtering_policy)
+
     kf = KFold(n_splits=num_folds, shuffle=True, random_state=43)
     folding = list(kf.split(dataset))
     train_subset_indices, validation_subset_indices = folding[fold]
@@ -108,8 +140,8 @@ def train_fold(num_folds, num_epochs, fold, model_name, dataset_name, optimizer_
     trainloader = torch.utils.data.DataLoader(train_subset, batch_size=64, shuffle=True)
     validationloader = torch.utils.data.DataLoader(validation_subset, batch_size=64, shuffle=False)
 
-    model_savers = [best_model.MINMetricValueModelSaver(savedir=logdir)]
-    last_epoch_model_saver = LastEpochModelSaver(savedir=logdir)
+    model_savers = [best_model.MINMetricValueModelSaver(savedir=experiment_dir)]
+    last_epoch_model_saver = LastEpochModelSaver(savedir=experiment_dir)
 
     for epoch in range(num_epochs):
         epoch_start_time = time.time()
@@ -125,6 +157,6 @@ def train_fold(num_folds, num_epochs, fold, model_name, dataset_name, optimizer_
     verbose('Model {} Fold {} Time: {:.2f} s'.format(model_name, fold, time.time() - fold_start_time), VERBOSE.LEVEL_1)
     queue.put(None)
 
-def logger(logdir, model_name, optimizer_name, queue):
-    dataframe_logger = DataframeLogger(logdir=logdir, metric_columns=[metric.name for metric in metrics], model_name=model_name, opt_name=optimizer_name)
+def logger(logdir, queue):
+    dataframe_logger = DataframeLogger(logdir=logdir, metric_columns=[metric.name for metric in metrics])
     dataframe_logger.start(queue)
