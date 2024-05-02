3a80a4f4830054cfdc544a61e1d273bc99b08744
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-05-02 10:38:16 +0330

    Add 'FILTERING_EXPERIMENT_COLS' in general settings

--- configs/general.py
+++ configs/general.py
@@ -19,6 +19,7 @@ BASE_DIR = "/home/vision/Repo/cleanset/logs"
 EXPERIMENT_BASE_DIR = osp.join(BASE_DIR, "basic_experiments")
 FILTERING_EXPERIMENT_BASE_DIR = osp.join(BASE_DIR, "filtering_experiments")
 EXPERIMENT_COLS = ["dataset", "model", "optim", "init", "lr_scheduler", "np", "ns", "lr" ]
+FILTERING_EXPERIMENT_COLS = ['basic_experiment_index', 'based_on', 'data_filtering_policy', 'data_retrieval_policy']
 
 EXPERIMENT_INFO_PATH = osp.join(BASE_DIR, "experiments.csv")
 FILTERING_EXPERIMENT_INFO_PATH = osp.join(BASE_DIR, "filtering_experiments.csv")
