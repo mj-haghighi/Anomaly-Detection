29fed3351932085447312e8cd197bbaa53ff01f9
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-05-02 21:33:47 +0330

    Implement 'AbstractCumulativeMetricClass' in metric level2 'abstract_class'

--- metric/level2/abstract_class.py
+++ metric/level2/abstract_class.py
@@ -69,3 +69,69 @@ class AbstractMeanStdMetricClass(ABC):
                 \n- `std`: std of calculated metric throw all epochs.
         """
         pass
+
+
+class AbstractCumulativeMetricClass(ABC):
+
+    def __init__(self, experiment_dir, phases, folds, epochs, epoch_skip=0, raw_dataset_path=""):
+        self.experiment_dir = experiment_dir
+        self.phases         = phases
+        self.folds          = folds
+        self.epochs         = epochs
+        self.epoch_skip     = epoch_skip
+        self.dataset_info   = pd.read_csv(raw_dataset_path, index_col='index')
+        self.scaler         = MinMaxScaler()
+
+
+    def calculate_auc_per_phase(self, metric, metric_name=None):
+        result = {}
+        for phase in self.phases:
+            phase_correctness = metric[metric['phase'] == phase]
+            merged_df = pd.merge(phase_correctness, self.dataset_info[['true_label']], left_on='sample', right_index=True, how='inner')
+            y_true = merged_df['true_label'] == merged_df['label']
+            if len(y_true.unique()) <= 1:
+                result[phase] = None
+            else:
+                auc_score = roc_auc_score(y_true=y_true, y_score = merged_df[metric_name if metric_name is not None else self.metric_name])
+                result[phase] = auc_score
+        return result
+
+
+    def calculate_metric_per_phase(self, scale=False):
+        metric = pd.DataFrame()
+        for phase in self.phases:
+            res = self.calculate_metric_on_folds(phase)
+            res['phase'] = phase
+            if scale:
+                res[self.metric_name] = self.scaler.fit_transform(res[[self.metric_name]])
+            metric = metric._append(res, ignore_index = True)
+        metric = metric.groupby(['sample', 'label', 'phase'])[self.metric_name].sum().reset_index()
+        return metric
+
+
+    def calculate_metric_on_folds(self, phase):
+        metric = pd.DataFrame()
+        for fold in range(self.folds):
+            res = self.calculate_metric_on_epochs(fold, phase)
+            metric = metric._append(res, ignore_index = True)
+        metric = metric.groupby(['sample', 'label'])[self.metric_name].sum().reset_index()
+        return metric
+
+
+    @property
+    @abstractmethod
+    def metric_name(self):
+        return "id2m"
+
+
+    @abstractmethod
+    def calculate_metric_on_epochs(self, fold, phase):
+        """
+        Calculate proposed metric per sample and provide `<metric_name>` columns in a resulting dataframe.
+        return:
+            `pd.Dataframe`: with columns [`sample`, `label`, `<metric_name>`].
+                \n- `sample`: sample index in dataset.
+                \n- `label`: label of sample in dataset.
+                \n- `<metric_name>`: calculated metric throw all epochs.
+        """
+        pass
