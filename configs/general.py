32325d6e897439310b89c1abf1592428e38343fc
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-05-10 13:11:58 +0330

    Add one more learning rate to both optimizers 'adam' and 'sgd', add two dropout rate, add two transform levels

--- configs/general.py
+++ configs/general.py
@@ -1,24 +1,26 @@
 import os.path as osp
 
 MODELS                          = ["resnet18", "resnet34", "xception"]
-DATASETS                        = ['cifar10']
-OPTIMS                          = [('adam', "0.001"), ('sgd', "0.1")]
+DATASETS                        = ["cifar10", "cifar100", "mnist"]
+OPTIMS                          = [('adam', ["0.001", "0.0001"]), ('sgd', ["0.1", "0.01"])]
 INITIALIZATIONS                 = ['pretrain', 'kaiming_normal']
-LR_SCHEDULERS                   = ['reduceLR', 'none', 'cosine_annealingLR']
+LR_SCHEDULERS                   = ['none'] #'reduceLR', 'cosine_annealingLR']
 NPS                             = ["0.0", "0.03", "0.07", "0.13"]
 NSS                             = ["0.0", "0.2", "0.4", "0.6"]
+DROPOUTS                        = ["0", "0.3"]
 PHASES                          = ['train', 'validation']
 DATA_FILTERING_POLICIES         = ['equal_to_np']
 DATA_RETRIEVAL_POLICIES         = ['remove', 'retrieve_noisy_labels', 'retrieve_all']
+TRANSFORM_LEVELS                = ['default', 'intermediate']
 EPOCHS                          = 15
 FOLDS                           = 3
 DEVICE                          = 'cuda:0'
 
 
-BASE_DIR = "/home/vision/Repo/cleanset/logs"
+BASE_DIR = "/home/vision/Repo/cleanset/logs2"
 EXPERIMENT_BASE_DIR = osp.join(BASE_DIR, "basic_experiments")
 FILTERING_EXPERIMENT_BASE_DIR = osp.join(BASE_DIR, "filtering_experiments")
-EXPERIMENT_COLS = ["dataset", "model", "optim", "transform", "init", "lr_scheduler", "np", "ns", "lr" ]
+EXPERIMENT_COLS = ["dataset", "model", "dropout", "optim", "lr", "lr_scheduler", "init", "transform", "np", "ns"]
 FILTERING_EXPERIMENT_COLS = ['basic_experiment_index', 'based_on', 'data_filtering_policy', 'data_retrieval_policy']
 
 EXPERIMENT_INFO_PATH = osp.join(BASE_DIR, "experiments.csv")
