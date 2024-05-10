95cc6cc7f761d5dabec3e6ba13afb2f88c6a7cbc
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-05-10 18:09:05 +0330

    Bugfix: use {} instead of [] in 'transform.Compose'

--- data/transforms/cifar10.py
+++ data/transforms/cifar10.py
@@ -12,9 +12,9 @@ default_validation_transform = tv_transforms.Compose([
     tv_transforms.Normalize(mean=Config.mean, std=Config.std),
 ])
 
-intermediate_train_transform = tv_transforms.Compose({
+intermediate_train_transform = tv_transforms.Compose([
     tv_transforms.ToTensor(),
     tv_transforms.RandomCrop(32, padding=4),
     tv_transforms.RandomHorizontalFlip(),
     tv_transforms.Normalize(mean=Config.mean, std=Config.std),
-})
+])
