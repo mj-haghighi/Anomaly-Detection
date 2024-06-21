72fae108a1bf5227285164982f63f85493234b6a
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-06-21 16:37:34 +0330

    Add some helper functions in 'utils/metrics'

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def calc_entropy(probabilities):
     return -np.sum(probabilities * np.log2(probabilities + 0.001))
@@ -20,4 +21,28 @@ def find_max_proba(prob_array):
     return np.max(prob_array)
 
 def argmax_list(lst):
-    return max(range(len(lst)), key=lst.__getitem__)    
+    return max(range(len(lst)), key=lst.__getitem__)
+
+def load_examins_auc(path, index_col):
+    experiments_with_corretness_auc = None
+    try:
+        experiments_with_corretness_auc = pd.read_csv(path, index_col=index_col)
+    except Exception as e:
+        print(e)
+    return experiments_with_corretness_auc
+
+def load_experiments(path, index_col):
+    experiments = pd.read_csv(path, index_col=index_col)
+    try:
+        experiments.drop(columns=['test_acc'], inplace=True)
+    except Exception as e:
+        print(e)
+    return experiments
+
+def filter_out_auc_calculated_experiments(experiments, experiments_with_corretness_auc):
+    experiments['has_auc'] = False
+    for idx in experiments_with_corretness_auc.index:
+        if idx in experiments.index:
+            experiments.loc[idx, 'has_auc'] = True
+
+    return experiments[(experiments['has_auc'] == False)]
