6398574ce0ddd817c0b776acba49f7f1122a25ab
mj-haghihgi <mohammadjavad.developer@gmail.com>
2024-08-25 09:33:42 +0330

    Set types in dataframe and save with compression

--- logger/dataframe.py
+++ logger/dataframe.py
@@ -2,6 +2,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import os
+import numpy as np
 import os.path as osp
 import pandas as pd
 from queue import Queue
@@ -34,7 +35,13 @@ class DataframeLogger(ILogger):
         directory = osp.join(self.logdir, f"{fold}", phase, f"{epoch :03d}")
         if not osp.exists(directory):
             os.makedirs(directory, exist_ok=True)
-        self.dataframe.to_pickle(osp.join(directory, f"{iteration :04d}-{self.base_name}"))
+
+        self.dataframe['sample'] = self.dataframe['sample'].astype('int32')
+        self.dataframe['label'] = self.dataframe['label'].astype('int8')
+        self.dataframe['loss'] = self.dataframe['loss'].astype('float16')
+        self.dataframe['proba'] = self.dataframe['proba'].apply(lambda x: x.astype(np.float16))
+
+        self.dataframe.to_pickle(osp.join(directory, f"{iteration :04d}-{self.base_name}"), compression="xz")
 
     def __log_sample(self, sample: str, label: str, metrics: Dict[str, float]):
         data = {"sample": sample, "label": label}
